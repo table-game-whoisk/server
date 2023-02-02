e8a7f053703a115cd8726fba234409aa107d768a
biubiujch <coolechi@foxmail.com>
2023-02-02 15:49:02 +0800

    update: express typescript jest dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello")
+import express from "express"
+import "./utils/parseENV"
+
+const app = express()
+const port = process.env.SERVER_PORT
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ src/test/index.test.ts
@@ -0,0 +1,8 @@
+import axios from "axios"
+
+
+
+it("hello world test", async () => {
+  const data = await axios.get("http://localhost:4000/")
+  expect(data.data).toBe('Hello World!');
+})
